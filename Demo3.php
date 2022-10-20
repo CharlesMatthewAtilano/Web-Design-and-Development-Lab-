4efec58faf0e9d0a81a7669507589507705d79d3
Charles Matthew Atilano <78058741+CharlesMatthewAtilano@users.noreply.github.com>
2022-10-20 20:58:57 +0800

    Add files via upload

---
+++ Demo3.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title> My First Web Page </title>
+<body align=center><h1>
+<?php
+//Equality and Inequality
+$a = 2;
+$b = 3;
+$month = "December";
+
+ if ($a===$b) echo "a is equal to b ";
+ if ($month=="December") echo:"It is Christmas Time!	";
+
+ if ($a>$b) echo"a greater than b";
+ if ($a<$b) echo"a lessthan b";
+ if ($a>=$b) echo"a greater than or equal b";
+ if ($a<=$b) echo"a less than or equal b";
+
+$c = 1;
+$d = 0;
+ echo ($c AND $d). 
+ echo ($c or $d).
+ echo ($c xor $d). 
+
+?></h1>
+</body>
+
+</head>
+
+</html>
